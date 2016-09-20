93aa403f4de08110b92cfc64be5c636f5b98ff37
IuryAlves <satriani-16@hotmail.com>
2016-09-20 00:16:45 -0300

    pep8

--- check_requirements_hook.py
+++ check_requirements_hook.py
@@ -7,43 +7,44 @@ import pkg_resources
 
 
 def _create_parser():
-	parser = argparse.ArgumentParser(description='Check if all requirements are installed.')
-	parser.add_argument('--requirements-file',
-	 				metavar='r',
-	 				type=str,
-	 				help='The requirements file to check',
-	 				default='requirements.txt')
-	return parser
+    parser = argparse.ArgumentParser(
+        description='Check if all requirements are installed.')
+    parser.add_argument('--requirements-file',
+                        metavar='r',
+                        type=str,
+                        help='The requirements file to check',
+                        default='requirements.txt')
+    return parser
 
 
 def open_requirements(requirements_file):
-	with open(requirements_file) as requirements:
-		return requirements.readlines()
+    with open(requirements_file) as requirements:
+        return requirements.readlines()
 
 
 def print_green(message):
-	template = '\033[92m{message}\033[0m'
-	print(template.format(message=message))
+    template = '\033[92m{message}\033[0m'
+    print(template.format(message=message))
 
 
 def print_red(message):
-	template = '\033[91m{message}\033[0m'
-	print(template.format(message=message))
+    template = '\033[91m{message}\033[0m'
+    print(template.format(message=message))
 
 
 def main():
-	
-	parser = _create_parser()
-	args = parser.parse_args()
-	dependencies = open_requirements(args.requirements_file)
-	try:
-		pkg_resources.require(dependencies)
-		print_green('Your requirements are up to date!')
-		sys.exit(0)
-	except (pkg_resources.DistributionNotFound, pkg_resources.VersionConflict):
-		print_red('You must update your requirements!')
-		sys.exit(1)
+
+    parser = _create_parser()
+    args = parser.parse_args()
+    dependencies = open_requirements(args.requirements_file)
+    try:
+        pkg_resources.require(dependencies)
+        print_green('Your requirements are up to date!')
+        sys.exit(0)
+    except (pkg_resources.DistributionNotFound, pkg_resources.VersionConflict):
+        print_red('You must update your requirements!')
+        sys.exit(1)
 
 
 if __name__ == '__main__':
-	main()
+    main()
